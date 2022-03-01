68248ea0411f507bfb294872d3152876b5e4c329
jacobayc <yaechan123@naver.com>
2022-03-01 19:45:29 +0900

    [zoom project] first commit

---
+++ src/server.js
@@ -0,0 +1,27 @@
+import http from "http";
+import WebSocket from "ws";
+import express from "express";
+
+const app = express();
+
+app.set("view engine", "pug");
+app.set("views", __dirname + "/public/views");
+app.use("/public", express.static(__dirname + "/public"));
+app.get("/", (req, res) => res.render("home"));
+app.get("/*", (req, res) => res.redirect("/"));
+
+const handleListen = () => console.log(`Listening on http://localhost:3000`);
+
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
+
+wss.on("connection", (socket) => {
+    console.log("Connected to Brouwser");
+    socket.on("close", () => {console.log("Disconnected from the Browser");});
+    socket.on("message", (message) => {
+        console.log(message.toString('utf8'));
+    })
+    socket.send("hello!!!")
+})
+
+server.listen(3000, handleListen);
